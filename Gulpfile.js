4aa823626354739a1c92c7bc090a8153fa70c721
Brad Dedon <bdedon@gmail.com>
2017-07-01 14:48:45 -0400

    Updates to styling. Added build process with Gulp and created distilled files for Bento.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('styles', function() {
+gulp.task('minifycss', function() {
   gulp.src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
     .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('js', function() {
+  return gulp.src([
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/jquery.min.js',
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/bootstrap/js/bootstrap.min.js',
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/js.cookie.min.js',
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js',
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/bootstrap-sweetalert/sweetalert.min.js',
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/bootstrap-modal/js/bootstrap-modalmanager.js',
+      './theme/metronic_v4.7.5/theme/assets/global/plugins/bootstrap-modal/js/bootstrap-modal.js',
+      './theme/metronic_v4.7.5/theme/assets/global/scripts/app.min.js',
+      './theme/metronic_v4.7.5/theme/assets/pages/scripts/ui-sweetalert.min.js',
+      './js/layout.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(uglify('app.min.js'))
+    .pipe( gulp.dest('./dist/'))
 });
 
 // Watch task
